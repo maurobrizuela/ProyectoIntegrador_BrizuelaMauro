b0785e7f13bf449d0625105e76914f5bd24f3c58
Mauro <maurobrizuela@hotmail.com>
2023-05-08 23:18:29 -0300

    EducationRunning

--- ProyectoBrizuela/FrontEnd/src/app/app.module.ts
+++ ProyectoBrizuela/FrontEnd/src/app/app.module.ts
@@ -19,6 +19,8 @@ import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 
 
 
@@ -37,7 +39,9 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     HomeComponent,
     LoginComponent,
     NewExperienciaComponent,
-    EditExperienciaComponent
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent
   ],
   imports: [
     BrowserModule,
