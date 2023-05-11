6e59434cafe9a6e4c42f1fc9f0a3c2ac3e2703b4
Mauro <maurobrizuela@hotmail.com>
2023-05-11 01:29:35 -0300

    Dockerfile

--- ProyectoBrizuela/FrontEnd/src/app/service/persona.service.ts
+++ ProyectoBrizuela/FrontEnd/src/app/service/persona.service.ts
@@ -7,11 +7,19 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = "http://localhost:8080/personas/";
+  persURL = "http://localhost:8080/personas/";
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL+'traer/perfil')
+  public list(): Observable<persona[]>{
+    return this.httpClient.get<persona[]>(this.persURL + 'lista');
+  }
+
+  public detail(id: number): Observable<persona>{
+    return this.httpClient.get<persona>(this.persURL + `detail/${id}`);
+  }
+
+  public update(id: number, Persona: persona): Observable<any>{
+    return this.httpClient.put<any>(this.persURL + `update/${id}`, Persona);
   }
 }
